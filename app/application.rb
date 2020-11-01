b93a920746eb0039bd68f6702b6c001fdfc84287
Samara Grant <samara.amanda01@gmail.com>
2020-11-01 21:04:33 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path == "/items/<Item Name>"
+      resp.write "#{item.price}"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+  end
+end
